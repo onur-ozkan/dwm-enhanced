c28b8fd8382deadecff806a930bc57c5e0b190a2
Onur Ozkan <onurozkan.dev@outlook.com>
2021-04-14 20:55:41 +0300

    provide status bar columns

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -1,9 +1,14 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%a, %B %d %I:%M %p'",					1,		0},
+	{" ", "./modules/memory",	3,		0},
+	{" ", "./modules/date",					3600,		0},
+	{" ", "./modules/clock",					3,		0},
+	// volume and battery have 
+	{" ", "./modules/brightness",	0,		11},
+	{"", "./modules/volume",	0,		10}, // this one have dynamic icon, so keep the icon column as blank value
+	{" ", "./modules/uptime", 60, 0},
+	{"", "./modules/battery", 5, 0}, // this one have dynamic icon, so keep the icon column as blank value
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
