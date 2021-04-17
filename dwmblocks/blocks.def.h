4c8b2029261793fddf8626a85a46d3088a550148
Onur Ozkan <onurozkan.dev@outlook.com>
2021-04-18 00:47:29 +0300

    add new icons to network module

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -3,14 +3,14 @@ static const Block blocks[] = {
 	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
 	{ " ", "./modules/kernel",		0,		0	},
 	{ " ", "./modules/cpu_temp",	3,		0	},
+//	{ " ", "./modules/date",		3600,	0	},
 	{ " ", "./modules/memory",		3,		0	},
-	{ "",	"./modules/network",	1,	    0   },
-	{ " ", "./modules/date",		3600,	0	},
-	{ " ", "./modules/clock",		3,		0	},
 	{ " ", "./modules/brightness",	0,		11	},
 	{ "", "./modules/volume",		0,		10	}, // this one have dynamic icon, so keep the icon column as blank value
+	{ "",	"./modules/network",	1,	    0   },
 	{ " ", "./modules/uptime",		60,		0	},
 	{ "", "./modules/battery",		3,		0	}, // this one have dynamic icon, so keep the icon column as blank value
+	{ " ", "./modules/clock",		3,		0	},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
