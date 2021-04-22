95020ccdbd0069dde4467f3b2e753818a6a061f2
onur <onurozkan.dev@outlook.com>
2021-04-22 19:48:09 -0400

    exclude scripts

--- dwmblocks/blocks.def.h
+++ dwmblocks/blocks.def.h
@@ -2,16 +2,16 @@
 
 static const Block blocks[] = {
 	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{ "./modules/kernel",		0,		0	},
-	{ "./modules/cpu_temp",	    3,		0	},
-	// { "./modules/date",		    3600,	0	},
-	{ "./modules/memory",		3,		0	},
-	{ "./modules/brightness",	0,		11	},
-	{ "./modules/volume",		0,		10	},
-	{ "./modules/network",	    1,	    0   },
-	{ "./modules/battery",		3,		0	},
-	{ "./modules/clock",		3,		0	},
-	{ "./modules/uptime",		60,		0	},
+	{ "dwm_kernel",		0,		0	},
+	{ "dwm_cpu_temp",	    3,		0	},
+	// { "dwm_date",		    3600,	0	},
+	{ "dwm_memory",		3,		0	},
+	{ "dwm_brightness",	0,		11	},
+	{ "dwm_volume",		0,		10	},
+	{ "dwm_network",	    1,	    0   },
+	{ "dwm_battery",		3,		0	},
+	{ "dwm_clock",		3,		0	},
+	{ "dwm_uptime",		60,		0	},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
