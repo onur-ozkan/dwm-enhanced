6beabd584237b22489d02cdd251fb56adfd544d7
Onur <onurozkan.dev@outlook.com>
2021-07-24 20:03:41 +0300

    Hide not-used tags and follow them while moving

--- dwm.c
+++ dwm.c
@@ -487,8 +487,8 @@ void attachstack(Client *c)
 
 void buttonpress(XEvent *e)
 {
-	unsigned int i, x, click;
-	Arg arg = {0};
+	unsigned int i, x, click, occ = 0;
+    Arg arg = {0};
 	Client *c;
 	Monitor *m;
 	XButtonPressedEvent *ev = &e->xbutton;
@@ -505,9 +505,14 @@ void buttonpress(XEvent *e)
 	if (ev->window == selmon->barwin)
 	{
 		i = x = 0;
-		do
+		for (c = m->clients; c; c = c->next)
+            occ |= c->tags == 255 ? 0 : c->tags;
+		do {
+			/* do not reserve space for vacant tags */
+			if (!(occ & 1 << i || m->tagset[m->seltags] & 1 << i))
+				continue;
 			x += TEXTW(tags[i]);
-		while (ev->x >= x && ++i < LENGTH(tags));
+		} while (ev->x >= x && ++i < LENGTH(tags));
 		if (i < LENGTH(tags))
 		{
 			click = ClkTagBar;
@@ -919,20 +924,20 @@ void drawbar(Monitor *m)
 
 	for (c = m->clients; c; c = c->next)
 	{
-		occ |= c->tags;
+        occ |= c->tags == 255 ? 0 : c->tags;
 		if (c->isurgent)
 			urg |= c->tags;
 	}
 	x = 0;
 	for (i = 0; i < LENGTH(tags); i++)
 	{
+        /* do not draw vacant tags */
+		if (!(occ & 1 << i || m->tagset[m->seltags] & 1 << i))
+		continue;
+
 		w = TEXTW(tags[i]);
 		drw_setscheme(drw, scheme[m->tagset[m->seltags] & 1 << i ? SchemeSel : SchemeNorm]);
 		drw_text(drw, x, 0, w, bh, lrpad / 2, tags[i], urg & 1 << i);
-		if (occ & 1 << i)
-			drw_rect(drw, x + boxs, boxs, boxw, boxw,
-					 m == selmon && selmon->sel && selmon->sel->tags & 1 << i,
-					 urg & 1 << i);
 		x += w;
 	}
 	w = blw = TEXTW(m->ltsymbol);
@@ -2051,6 +2056,8 @@ void tag(const Arg *arg)
 		selmon->sel->tags = arg->ui & TAGMASK;
 		focus(NULL);
 		arrange(selmon);
+        if(viewontag && ((arg->ui & TAGMASK) != TAGMASK))
+			view(arg);
 	}
 }
 
