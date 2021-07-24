10296fb53e03a84106976411a0c99e6ac873274c
Onur Ozkan <onurozkan.dev@outlook.com>
2021-07-24 15:14:03 +0300

    decrease font-size to 11px

--- config.h
+++ config.h
@@ -10,8 +10,8 @@ static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 static const int focusonwheel       = 0;
-static const char *fonts[]          = { "RobotoMono Nerd Font:size=12" };
-static const char dmenufont[]       = "Roboto Mono:size=12";
+static const char *fonts[]          = { "RobotoMono Nerd Font:size=11" };
+static const char dmenufont[]       = "Roboto Mono:size=11";
 static const char col_soft_black[]  = "#222222";
 static const char col_gray[]        = "#616161";
 static const char col_dirty_white[] = "#eeeeee";
