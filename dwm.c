b72920e8dc2d13e80a34fa1f61d67a6755c19300
Onur <onurozkan.dev@outlook.com>
2021-11-04 12:53:02 +0300

    fix glitching on statusbar

--- dwm.c
+++ dwm.c
@@ -970,7 +970,7 @@ void drawbar(Monitor *m)
 		if (m->sel)
 		{
 			drw_setscheme(drw, scheme[m == selmon ? SchemeSel : SchemeNorm]);
-			drw_text(drw, x, 0, w - 2 * sp, bh, lrpad / 2, m->sel->name, 0);
+			drw_text(drw, x, 0, w, bh, lrpad / 2, m->sel->name, 0);
 
 			if (m->sel->isfloating)
 				drw_rect(drw, x + boxs, boxs, boxw, boxw, m->sel->isfixed, 0);
